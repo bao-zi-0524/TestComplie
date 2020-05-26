bdea0a236b6ce7974c14f882b8aa32d02317f665
tianbao <tianbao@bbdtek.com>
2020-05-26 17:38:17 +0800

    Initial commit

---
+++ testcomplie/src/main/java/com/urty/testcomplie/test.java
@@ -0,0 +1,14 @@
+package com.urty.testcomplie;
+
+public class test {
+    public String getSss() {
+        return sss;
+    }
+
+    public void setSss(String sss) {
+        this.sss = sss;
+    }
+
+    private String sss;
+
+}
